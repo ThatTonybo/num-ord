17084794d01cd690496edff878b67bc0cd48133d
Tony <twatt74@eq.edu.au>
2018-10-19 11:19:35 +1000

    Delete Errors.js

--- src/models/Errors.js
+++
@@ -1,7 +0,0 @@
-const Enum = require('enum');
-
-const Errors = new Enum({
-    'INVALID_TYPE': { code: 0, plain: `The provided variable was not a supported type, only numbers are supported.` },
-});
-
-module.exports = Errors;
